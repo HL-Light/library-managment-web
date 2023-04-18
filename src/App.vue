5865a9f85ca353256745e915c3620ddfb785ef34
HL-Light <li2053660167@gmail.com>
2023-04-18 20:00:07 +0800

    初次更新主体

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <!--头部-->
+    <div style="height: 60px; line-height: 60px; background-color: white">
+      <img src="@/assets/logo-head.png" alt="" style="width: 40px; position: relative; top: 10px; left: 10px">
+      <span style="margin-left: 20px; font-size: 24px;">图书管理系统</span>
+    </div>
+
+    <!--侧边栏主体-->
+    <div style="display: flex">
+      <!--侧边栏导航-->
+      <div style="width: 200px; overflow: hidden; margin-right: 2px; background-color: white">
+        <el-menu :default-active="$route.path" class="el-menu-demo">
+          <el-menu-item index="1">处理中心</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </div>
+
+      <!--主体数据-->
+      <div><el-button type="primary">这是一个按钮</el-button></div>
+    </div>
+
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
