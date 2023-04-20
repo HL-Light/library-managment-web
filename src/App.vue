80a04f73fbfe0cec948e38e00e451e578d057c97
HL-Light <li2053660167@gmail.com>
2023-04-20 21:56:56 +0800

    主体完工

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,6 @@
 <template>
   <div id="app">
-    <!--头部-->
-    <div style="height: 60px; line-height: 60px; background-color: white">
-      <img src="@/assets/logo-head.png" alt="" style="width: 40px; position: relative; top: 10px; left: 10px">
-      <span style="margin-left: 20px; font-size: 24px;">图书管理系统</span>
-    </div>
-
-    <!--侧边栏主体-->
-    <div style="display: flex">
-      <!--侧边栏导航-->
-      <div style="width: 200px; overflow: hidden; margin-right: 2px; background-color: white">
-        <el-menu :default-active="$route.path" class="el-menu-demo">
-          <el-menu-item index="1">处理中心</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-          </el-submenu>
-        </el-menu>
-      </div>
-
-      <!--主体数据-->
-      <div><el-button type="primary">这是一个按钮</el-button></div>
-    </div>
-
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import SlideVerify from 'vue-monoplasty-slide-verify';
+Vue.use(SlideVerify);
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
